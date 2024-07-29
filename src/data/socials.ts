5e9a2f5b366b31518750db6fd4ed0989fa771455
Snoparino <aboutthathell@gmail.com>
2024-07-29 06:36:19 +0300

    edited everything

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,21 @@ export const socials = [
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
+    url: 'https://www.instagram.com/KatchupAg',
   },
   {
     title: 'Twitter',
     icon: 'mdi:twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/KatchupAg',
   },
   {
     title: 'YouTube',
     icon: 'mdi:youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    url: 'https://youtube.com/c/KatchupAg',
   },
   {
-    title: 'GitHub',
-    icon: 'mdi:github',
-    url: 'https://github.com/littlesticksdev',
-  },
-  {
-    title: 'Dribbble',
-    icon: 'mdi:dribbble',
-    url: 'https://dribbble.com/littlesticksdev',  
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://facebook.com/KatchupAg',
   }
 ]
